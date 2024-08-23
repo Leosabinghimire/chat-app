1a6853ec907dbbe11d167b60b5e994137c2f05b5
leosabinghimire <sabinghimire2055@gmail.com>
2024-08-23 13:54:31 +0545

    error fix

--- src/context/Appcontext.jsx
+++ src/context/Appcontext.jsx
@@ -1,6 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { auth, db } from "../config/firebase";
 import { doc, getDoc, updateDoc } from "firebase/firestore/lite";
+import { onSnapshot } from "firebase/firestore";
+
 import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
@@ -10,19 +12,23 @@ const AppContextProvider = (props) => {
   const [userData, setUserData] = useState(null);
   const [chatData, setChatData] = useState(null);
 
-  const loadUserData = async (uid) => {
+  const loadUserData = async (uid, isNewAccount = false) => {
     try {
-      const userRef = doc(db, "user", uid);
+      const userRef = doc(db, "users", uid);
       const userDoc = await getDoc(userRef);
       const userData = userDoc.data();
 
       if (userData) {
         setUserData(userData);
 
-        if (userData.avatar && userData.name) {
-          navigate("/chat");
+        if (isNewAccount) {
+          navigate("/profile"); // Navigate to profile if it's a new account
         } else {
-          navigate("/profile");
+          if (userData.avatar && userData.name) {
+            navigate("/chat");
+          } else {
+            navigate("/profile");
+          }
         }
 
         await updateDoc(userRef, { lastSeen: Date.now() });
@@ -33,12 +39,32 @@ const AppContextProvider = (props) => {
           }
         }, 60000);
       } else {
-        // console.log("No user data found");
+        // Handle case where no user data is found
       }
     } catch (error) {
       console.error("Error loading user data:", error);
     }
   };
+
+  useEffect(() => {
+    if (userData) {
+      const chatRef = doc(db, "chats", userData.id);
+      const unSub = onSnapshot(chatRef, async (res) => {
+        const chatItems = res.data().chatData;
+        const tempData = [];
+        for (const item of chatItems) {
+          const userRef = doc(db, "users", item.rid);
+          const usersnap = await getDoc(userRef);
+          const userData = usersnap.data();
+          tempData.push({ ...item, userData });
+        }
+        setChatData(tempData.sort((a, b) => b.updatedAt - a.updatedAt));
+      });
+      return () => {
+        unSub();
+      };
+    }
+  });
 
   const value = {
     userData,
